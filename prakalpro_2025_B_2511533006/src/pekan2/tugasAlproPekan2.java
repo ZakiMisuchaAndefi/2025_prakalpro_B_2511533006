4dfa60aeddb6075f22e316175032b74bda349f23
Zaki Misucha Andefi <Zaki Misucha Andefi@LAPTOP-7VE7RV67>
2025-09-21 13:15:26 +0700

    Tugas Alpro Pekan 2

---
+++ prakalpro_2025_B_2511533006/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,23 @@
+package pekan2;
+
+public class tugasAlproPekan2 {
+	    public static void main(String[] args) {
+	        // Deklarasi variabel dengan tipe data yang diminta
+	    	// contoh: 655.200 unit
+	    	int jumlahUnitTerjual = 655200;
+	        // pendapatan Samsung Q1 2025 dalam triliun won
+	        float pendapatanDalamTriliunWon = 79.14f; 
+	        // misalnya 'Z Fold' untuk Galaxy Z Fold7
+	        char kodeProdukTerlaris = 'Z';
+	        // apakah penjualan meningkat dari awal perilisan
+	        boolean penjualanMeningkat = true; 
+
+	        // Menampilkan data atau hasil
+	        System.out.println("=== Laporan Penjualan Samsung Galaxy Z Fold7 2025 ===");
+	        System.out.println("Jumlah Unit Terjual       : " + jumlahUnitTerjual + " unit");
+	        System.out.println("Pendapatan                : " + pendapatanDalamTriliunWon + " triliun won");
+	        System.out.println("Kode Produk Terlaris      : " + kodeProdukTerlaris);
+	        System.out.println("Penjualan Meningkat?      : " + penjualanMeningkat);
+	        
+	    }
+	}
